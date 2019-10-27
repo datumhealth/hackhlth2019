7c48d29ee190d3ca1cdfa718198f4e59de857541
Ian Macalinao <me@ian.pw>
2019-10-27 00:49:37 -0700

    format

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,6 +10,12 @@ export const CardInner = styled.div`
   border-bottom: 1px solid #f0f2f4;
 `;
 
+export const CardInnerCenter = styled(CardInner)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const Card = styled(CardNoPadding)`
   padding: 23px;
 `;
