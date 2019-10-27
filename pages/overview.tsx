172ebbd662f6df61ab827a1b8f777e816d8d560f
Ian Macalinao <me@ian.pw>
2019-10-26 23:05:39 -0700

    table header

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { Icon, Table } from "antd";
+import { Icon, Table, Button } from "antd";
 import { NextPage } from "next";
 import { Card } from "../components/Card";
 import { COLUMNS, ROWS } from "../components/sampleData";
@@ -123,11 +123,37 @@ const Wrapper = styled.div`
   padding: 33px 23px;
 `;
 
+const TableHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  .anticon-area-chart {
+    color: #1890ff;
+  }
+`;
+
+const Actions = styled.div`
+  button {
+    margin-right: 15px;
+  }
+  padding-bottom: 17px;
+`;
+
 const Page: NextPage<Props> = ({ userAgent }) => (
   <div>
     <Navbar />
     <Wrapper>
       <Card>
+        <TableHeader>
+          <Actions>
+            <Button icon="filter" size="large">
+              Filters
+            </Button>
+            <Button icon="caret-down" size="large">
+              Sort by
+            </Button>
+          </Actions>
+          <Button icon="area-chart" size="large"></Button>
+        </TableHeader>
         <Table columns={COLUMNS} dataSource={ROWS} bordered={true} />
       </Card>
     </Wrapper>
