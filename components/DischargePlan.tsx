13b8956b889203712df2fe49c6bdf8c7ebbed25f
Ian Macalinao <me@ian.pw>
2019-10-27 10:49:09 -0700

    file plan

--- components/DischargePlan.tsx
+++ components/DischargePlan.tsx
@@ -1,6 +1,50 @@
-import { DischargePlanGuy } from "./DischargePlanGuy";
-import { Button } from "antd";
 import styled from "@emotion/styled";
+import { Button, Form, message, Modal, Select } from "antd";
+import { useState } from "react";
+import { DischargePlanGuy } from "./DischargePlanGuy";
+
+export const DischargeModal = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+  return (
+    <>
+      <Button
+        type="primary"
+        size="large"
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        File Discharge Plan
+      </Button>
+      <Modal
+        title="File a Discharge Plan"
+        visible={visible}
+        onOk={() => {
+          setVisible(false);
+        }}
+        onCancel={() => setVisible(false)}
+        okText="File"
+      >
+        <DWrap>
+          <Button size="large">Upload a Form</Button>
+          <Or>or</Or>
+          <Button size="large">Fill Out Form Online</Button>
+        </DWrap>
+      </Modal>
+    </>
+  );
+};
+
+const DWrap = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Or = styled.span`
+  margin-top: 13px;
+  margin-bottom: 13px;
+`;
 
 const Wrapper = styled.div`
   text-align: center;
@@ -14,8 +58,6 @@ export const DischargePlan = () => (
     <DischargePlanGuy />
     <h3>You have not yet filed a Discharge Plan.</h3>
     <p>To file a Discharge Plan, click the button below.</p>
-    <Button size="large" type="primary">
-      File Discharge Plan
-    </Button>
+    <DischargeModal />
   </Wrapper>
 );
