a7324092f8dc651f53b8c85ea9023a8586a45242
Ian Macalinao <me@ian.pw>
2019-10-27 11:50:26 -0700

    wrap

--- pages/mobile/add_diary.tsx
+++ pages/mobile/add_diary.tsx
@@ -1,25 +1,15 @@
 import styled from "@emotion/styled";
 import { NextPage } from "next";
 import Router from "next/router";
-import { css, Global } from "@emotion/core";
 
 const Wrap = styled.div`
   display: flex;
   justify-content: center;
-  height: 100vh;
-  width: 100wh;
-  background-color: #c4c4c4;
+  margin-top: 40px;
 `;
 
 const Page: NextPage<{}> = () => (
   <Wrap>
-    <Global
-      styles={css`
-        body {
-          margin: 0;
-        }
-      `}
-    />
     <img src="/adddiary.svg" onClick={() => Router.push("/mobile/story")} />
   </Wrap>
 );
